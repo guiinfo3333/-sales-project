cf3d79cc544cf95f4360bfc90344a6b0d627b200
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2020-05-07 16:52:22 -0300

    teste

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ dialect: 'mysql',
 host: 'mysql669.umbler.com',
 username: 'rootfut',
 password: '12345678a',
-database: 'Sales',
+database: 'sales',
 define : {
 	timestamps: true,
 	underscored: true,

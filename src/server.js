39dddeed44674abcbfff73ba19bec9d09c2d1e22
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2020-05-08 10:24:57 -0300

    deixando imagem acessivel

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const morgan = require("morgan");  //para log
 const cors = require('cors');
+const path = require("path");
 
 require('./database');
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
+app.use('/files',express.static(path.resolve(__dirname,'..','tmp','uploads')))
 
 app.use(routes);
 app.listen(3000);
